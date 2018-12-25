dac756bd6f37c6d462bf514ba25e84dbede01668
leno <lam.do@asiantech.vn>
2018-12-25 16:15:11 +0700

    Management text of general display

---
+++ resources/lang/en/master.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Define Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+    'title' => 'Computer Shop Admin',
+    'header' => [
+        'title' => 'Computer Shop',
+        'admin' => 'Admin',
+        'logout' => 'Logout',
+    ],
+    'sidebar' => [
+        'home' => 'Home',
+        'user' => 'Users Management',
+        'role' => 'Roles Management',
+        'category' => 'Categories Management',
+        'product' => 'Products Management',
+        'image' => 'Images Management',
+        'img_category' => 'Categories',
+        'img_product' => 'Products',
+        'comment' => 'Comments Management',
+        'order' => 'Orders Management',
+    ],
+    'footer' => [
+        'design' => 'Design by',
+        'year' => '2017-2019',
+        'author' => 'Bootstrapious',
+    ]
+];
---
+++ resources/views/admin/layout/sidebar.blade.php
@@ -0,0 +1,26 @@
+<!-- Side Navbar -->
+<nav class="side-navbar">
+  <!-- Sidebar Header-->
+  <div class="sidebar-header d-flex align-items-center">
+    <div class="avatar"><img src="admin_asset/img/avatar-1.jpg" alt="..." class="img-fluid rounded-circle"></div>
+    <div class="title">
+      <h1 class="h4">Mark Stephen</h1>
+    </div>
+  </div>
+  <!-- Sidebar Navidation Menus-->
+  <ul class="list-unstyled">
+    <li><a href="/admin/home"><i class="fa fa-home"></i>@lang('master.sidebar.home')</a></li>
+    <li><a href="/admin"><i class="fa fa-users"></i>@lang('master.sidebar.user')</a></li>
+    <li><a href="/admin"><i class="fa fa-universal-access"></i>@lang('master.sidebar.role')</a></li>
+    <li><a href="login.html"> <i class="fa fa-align-justify"></i>@lang('master.sidebar.category')</a></li>
+    <li><a href="login.html"> <i class="fa fa-industry"></i>@lang('master.sidebar.product')</a></li>
+    <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"><i class="fa fa-image"></i>@lang('master.sidebar.image')</a>
+      <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
+        <li><a href="#">@lang('master.sidebar.img_category')</a></li>
+        <li><a href="#">@lang('master.sidebar.img_product')</a></li>
+      </ul>
+    </li>
+    <li><a href="login.html"><i class="fa fa-comments"></i>@lang('master.sidebar.comment')</a></li>
+    <li><a href="login.html"><i class="fa fa-shopping-cart"></i>@lang('master.sidebar.order')</a></li>
+  </ul>
+</nav>
