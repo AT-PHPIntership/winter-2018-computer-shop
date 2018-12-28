f0695211dd37372d6e5930542753041d616b8f7c
Thanh Nguyen H. [2] <thanh.nguyen2@asiantech.vn>
2018-12-28 10:14:33 +0700

    change errorr view composer

--- app/Http/ViewComposers/RoleComposer.php
+++ app/Http/ViewComposers/RoleComposer.php
@@ -5,17 +5,17 @@ namespace App\Http\ViewComposers;
 use Illuminate\View\View;
 use App\Models\Role;
 
-
 class RoleComposer
 {
-   /**
+    /**
     * Bind data to the view.
     *
-    * @param  View  $view
+    * @param View $view [view]
+    *
     * @return void
     */
-   public function compose(View $view)
-   {
-       $view->with('roles', Role::select('id', 'name')->paginate(5));
-   }
+    public function compose(View $view)
+    {
+        $view->with('roles', Role::select('id', 'name')->paginate(5));
+    }
 }
