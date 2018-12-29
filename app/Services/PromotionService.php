e27a215f7f441309bd0b7ab720223e2664e7f6bd
Thanh Nguyen H. [2] <thanh.nguyen2@asiantech.vn>
2018-12-29 17:31:42 +0700

    handle promotion create

--- app/Services/PromotionService.php
+++ app/Services/PromotionService.php
@@ -16,4 +16,21 @@ class PromotionService
         $promotions = Promotion::all();
         return $promotions;
     }
+
+    /**
+     * Create Promotion
+     *
+     * @param object $request Request from form
+     *
+     * @return void
+     */
+    public function create($request)
+    {
+        Promotion::create([
+            'name' => $request->name,
+            'percent' => $request->percent,
+            'start_at' => $request->start_at,
+            'end_at' => $request->end_at
+        ]);
+    }
 }
